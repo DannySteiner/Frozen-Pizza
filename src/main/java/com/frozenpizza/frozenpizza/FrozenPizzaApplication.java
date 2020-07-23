418872d68789ca47b4db9383b0baf8493907c67e
Scrambles <danny.steiner1@gmail.com>
2020-07-23 19:12:05 -0400

    Initial commit

---
+++ src/main/java/com/frozenpizza/frozenpizza/FrozenPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.frozenpizza.frozenpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrozenPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrozenPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frozenpizza/frozenpizza/FrozenPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frozenpizza.frozenpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrozenPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
